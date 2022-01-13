92e77d6e9c6ea3947261f57179cd416c42721884
CMD SHAKEEL <cmdshakeel21@gmail.com>
2022-01-13 15:27:33 +0530

    created web application pages using html and css

---
+++ empwage.js
@@ -0,0 +1,13 @@
+function calcuDailWage(empHrs){
+    return empHrs * Wage_per_Hour;
+
+}
+while(totalHours<= Max_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+    totalWorkingDays++;
+    let empCheck = Math
+}
+const Max_HRS_IN_MONTH =160;
+const NUM_OF_WORKING_DAYS = 20;
+let totalEmpHrs = 0;
+
+console.log(Max_HRS_IN_MONTH+NUM_OF_WORKING_DAYS);
